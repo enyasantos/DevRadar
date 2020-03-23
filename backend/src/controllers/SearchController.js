d73f2ac76d222ceaf73e3390bd94c4cbd1865741
Enya <enyalgs@yahoo.com>
2020-03-23 02:27:28 -0300

    v1.0

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,29 @@
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+    async index(request, response) {
+        const { latitude, longitude, techs } = request.query;
+
+        const techsArray = parseStringAsArray(techs);
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000,
+                }
+            }
+        });
+
+        //buscar todos os devs em um raio de 10km
+        //filtrar por tecnologias
+        return response.json( { devs });
+    }
+}
---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://enya:zonevir@cluster0-8iza7.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+//Métodos HTTP: get, post, put, delete
+
+//  Tipos de parâmetros
+//  Query Params: parâmetro enviando da url - request.query (Filtros, ordenação, paginação, ... )
+//  Route Params: request.params (identificara um recurso na alteração ou remoção)
+//  Body: request.body (Dados para criação ou alteração de um registro)
+
+//  MongoDB (Não-relacional)
+
+
+app.listen(3001);
